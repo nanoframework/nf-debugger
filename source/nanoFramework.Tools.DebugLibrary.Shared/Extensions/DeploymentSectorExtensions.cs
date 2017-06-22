cd1c4055f2be4b4ba8d5a016dc4b4810fc71aa45
José Simões <josesimoes@users.noreply.github.com>
2017-06-22 02:58:08 +0100

    Work on DeploymentExecuteIncrementalAsync (#27)
    - rework method to deploy using flash map
    - add code and extension as required
    - bumped Nuget version to 0.4.0-preview013
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/nanoFramework.Tools.DebugLibrary.Shared/Extensions/DeploymentSectorExtensions.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using nanoFramework.Tools.Debugger.WireProtocol;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace nanoFramework.Tools.Debugger.Extensions
+{
+    public static class DeploymentSectorExtensions
+    {
+        public static List<DeploymentBlock> ToDeploymentBlockList(this List<DeploymentSector> value)
+        {
+            List<DeploymentBlock> blocks = new List<DeploymentBlock>();
+
+            foreach(DeploymentSector sector in value)
+            {
+                blocks.AddRange(sector.Blocks);
+            }
+
+            return blocks;
+        }
+    }
+}
