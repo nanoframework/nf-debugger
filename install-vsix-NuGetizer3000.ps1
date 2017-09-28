f9366c7d3373192a3722e6270f1ec493eac57c41
Robin Jones <robin@networkfusion.co.uk>
2017-09-28 20:52:00 +0100

    updated ps to have correct filename

--- install-vsix-NuGetizer3000.ps1
+++ install-vsix-NuGetizer3000.ps1
@@ -1,4 +1,4 @@
-$vsixPath = "$($env:USERPROFILE)\nanoFramework.Tools.VS2017.Extension.vsix"
+$vsixPath = "$($env:USERPROFILE)\NuGetizer3000-VS2017.vsix"
 (New-Object Net.WebClient).DownloadFile('http://bit.ly/nugetizer-2017', $vsixPath)
 "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 & .\install-vsix.cmd
