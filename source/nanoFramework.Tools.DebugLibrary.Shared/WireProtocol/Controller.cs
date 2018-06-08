53e566d4afbd3aa8a14505a502879dc9cf96de39
José Simões <jose.simoes@eclo.solutions>
2018-06-08 15:12:41 +0100

    Improvements in engine process exit and termination
    - Remove EngineState as we dont't need that anymore: incomming messages are handled on a Task
    - Rework Debugger.Engine accordingly
    - Replace checks related with EngineState with ones using _backgroundProcessor Task
    - Improve code related with calls to ProcessExit on device failure
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/nanoFramework.Tools.DebugLibrary.Shared/WireProtocol/Controller.cs
+++ source/nanoFramework.Tools.DebugLibrary.Shared/WireProtocol/Controller.cs
@@ -38,6 +38,11 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             return new Converter(Capabilities);
         }
 
+        private void ProcessExit()
+        {
+            App.ProcessExited();
+        }
+
         public async Task<bool> SendAsync(MessageRaw raw, CancellationToken cancellationToken)
         {
             _sendSemaphore.WaitOne();
@@ -94,6 +99,10 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             catch (TaskCanceledException)
             {
                 // don't do anything here, as this is expected
+            }
+            catch(DeviceNotConnectedException)
+            {
+                App.ProcessExited();
             }
             finally
             {
@@ -166,8 +175,13 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             {
                 // don't do anything here, as this is expected
             }
+            catch (DeviceNotConnectedException)
+            {
+                App.ProcessExited();
+            }
 
             return bytesToReadRequested - bytesToRead;
         }
+
     }
 }
