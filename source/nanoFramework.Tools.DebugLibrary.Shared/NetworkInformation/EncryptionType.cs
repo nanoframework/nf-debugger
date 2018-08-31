4f42d4865825cecca975908283ebc830d91b215c
José Simões <jose.simoes@eclo.solutions>
2018-08-31 15:21:30 +0100

    Add Display.Description atrribute to network enums
    - Improve NetworkWirelessConfiguration constructor and PrepareForDeserialize
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/nanoFramework.Tools.DebugLibrary.Shared/NetworkInformation/EncryptionType.cs
+++ source/nanoFramework.Tools.DebugLibrary.Shared/NetworkInformation/EncryptionType.cs
@@ -4,6 +4,8 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System.ComponentModel.DataAnnotations;
+
 namespace nanoFramework.Tools.Debugger
 {
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -14,30 +16,43 @@ namespace nanoFramework.Tools.Debugger
         /// <summary>
         /// No encryption.
         /// </summary>
+        [Display(Description = "")]
         None = 0,
+
         /// <summary>
         /// Wired Equivalent Privacy encryption.
         /// </summary>
+        [Display(Description = "WEP")]
         WEP,
+
         /// <summary>
         /// Wireless Protected Access encryption.
         /// </summary>
+        [Display(Description = "WPA")]
         WPA,
+
         /// <summary>
         /// Wireless Protected Access 2 encryption.
         /// </summary>
+        [Display(Description = "WPA2")]
         WPA2,
+
         /// <summary>
         /// Wireless Protected Access Pre-Shared Key encryption.
         /// </summary>
+        [Display(Description = "WPA Pre-Shared Key")]
         WPA_PSK,
+
         /// <summary>
         /// Wireless Protected Access 2 Pre-Shared Key encryption.
         /// </summary>
-        WPA2_PSK,
+        [Display(Description = "WPA2 Pre-Shared Key")]
+        WPA2_PSK2,
+
         /// <summary>
         /// Certificate encryption.
         /// </summary>
+        [Display(Description = "Certificate")]
         Certificate,
     }
 }
--- source/nanoFramework.Tools.DebugLibrary.Shared/NetworkInformation/NetworkInterfaceType.cs
+++ source/nanoFramework.Tools.DebugLibrary.Shared/NetworkInformation/NetworkInterfaceType.cs
@@ -4,6 +4,8 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System.ComponentModel.DataAnnotations;
+
 namespace nanoFramework.Tools.Debugger
 {
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -19,11 +21,13 @@ namespace nanoFramework.Tools.Debugger
         /// <summary>
         /// The network interface uses an Ethernet connection. Ethernet is defined in IEEE standard 802.3.
         /// </summary>
+        [Display(Description = "Ethernet")]
         Ethernet = 6,
 
         /// <summary>
         /// The network interface uses a wireless LAN connection (IEEE 802.11 standard).
         /// </summary>
+        [Display(Description = "Wi-Fi (802.11)")]
         Wireless80211 = 71,
     }
 }
