056382c875596ac6d811c5dba2e40b0f71159131
José Simões <jose.simoes@eclo.solutions>
2018-11-07 18:15:03 +0000

    Work CD-CI
    - Replace gitversion with Nerdbank.GitVersioning.
    - Update AppVeyor yaml and other PS scripts accordingly.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/nanoFramework.Tools.DebugLibrary.Net/Properties/AssemblyInfo.cs
+++ source/nanoFramework.Tools.DebugLibrary.Net/Properties/AssemblyInfo.cs
@@ -6,22 +6,15 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nanoFramework Debug Library")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The nanoFramework project contributors")]
 [assembly: AssemblyProduct("nanoFramework Debug Library")]
 [assembly: AssemblyCopyright("Copyright © 2017 nanoFramework project contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("101d57ad-d22f-4905-a992-de15e723f164")]
-
-////////////////////////////////////////////////
-// Assembly*Version managed by GitVersionTask //
+////////////////////////////////////////////////////////
+// Assembly*Version managed by Nerdbank.GitVersioning //
 ////////////////////////////////////////////////
