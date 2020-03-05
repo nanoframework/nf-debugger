343795b6f30e5ac72bcc00afe014e0715519092f
josesimoes <jose.simoes@eclo.solutions>
2020-03-05 17:43:15 +0000

    Work CD-CI
    - Fix restore command with nuget.config file.
    - Improve console output.
    
    ***UPDATE_DEPENDENTS***
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- azure-pipelines/update-dependencies.ps1
+++ azure-pipelines/update-dependencies.ps1
@@ -38,20 +38,24 @@ Set-Location source | Out-Null
 ####################
 # VS 2017
 
-Write-Host "Updating package in VS2017 solution..."
+Write-Host "Restoring packages in VS2017 solution..."
 
 # restore NuGet packages, need to do this before anything else
-nuget restore nanoFramework.Tools.VisualStudio.sln -Source https://pkgs.dev.azure.com/nanoframework/feed/_packaging/sandbox/nuget/v3/index.json
+nuget restore nanoFramework.Tools.VisualStudio.sln -ConfigFile NuGet.Config
+
+Write-Host "Updating nanoFramework.Tools.Debugger.Net package in VS2017 solution..."
 
 nuget update -Id nanoFramework.Tools.Debugger.Net VisualStudio.Extension\VisualStudio.Extension.csproj -ConfigFile NuGet.Config -PreRelease
 
 ####################
 # VS 2019
 
-Write-Debug "Updating package in VS2019 solution..."
+Write-Host "Restoring packages in VS2019 solution..."
 
 # restore NuGet packages, need to do this before anything else
-nuget restore nanoFramework.Tools.VisualStudio-2019.sln -Source https://pkgs.dev.azure.com/nanoframework/feed/_packaging/sandbox/nuget/v3/index.json
+nuget restore nanoFramework.Tools.VisualStudio-2019.sln -ConfigFile NuGet.Config
+
+Write-Host "Updating nanoFramework.Tools.Debugger.Net package in VS2019 solution..."
 
 nuget update -Id nanoFramework.Tools.Debugger.Net VisualStudio.Extension-2019\VisualStudio.Extension.csproj -ConfigFile NuGet.Config -PreRelease
 
