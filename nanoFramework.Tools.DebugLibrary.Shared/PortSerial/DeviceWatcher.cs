53a04f74af43ea108894227c9842b35601dde50a
José Simões <jose.simoes@eclo.solutions>
2021-04-14 11:13:06 +0100

    Add device watcher class

---
+++ nanoFramework.Tools.DebugLibrary.Shared/PortSerial/DeviceWatcher.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace nanoFramework.Tools.Debugger.PortSerial
+{
+    public class DeviceWatcher
+    {
+        private bool _started = false;
+        private readonly List<string> _ports = new List<string>();
+
+        public delegate void EventDeviceAdded(object sender, string port);
+
+        public event EventDeviceAdded Added;
+
+        public delegate void EventDeviceRemoved(object sender, string port);
+
+        public event EventDeviceRemoved Removed;
+
+        public delegate void EventEnumerationCompleted(object sender, List<string> ports);
+
+        public event EventEnumerationCompleted EnumerationCompleted;
+
+        public DeviceWatcherStatus Status { get; internal set; }
+
+        public void Start()
+        {
+            if (!_started)
+            {
+                var threadWatch = new Thread(() =>
+                {
+                    _started = true;
+                    Status = DeviceWatcherStatus.Started;
+                    bool changed = true; ;
+                    while (_started)
+                    {
+                        var ports = SerialPort.GetPortNames();
+                        
+                        foreach (var port in ports)
+                        {
+                            if (!_ports.Contains(port))
+                            {
+                                _ports.Add(port);
+                                Added?.Invoke(this, port);
+                                changed = true;
+                            }
+                        }
+
+                        List<string> portsToRemove = new List<string>();
+
+                        foreach(var port in _ports)
+                        {
+                            if(!ports.Contains(port))
+                            {
+                                portsToRemove.Add(port);
+                            }
+                        }
+
+                        foreach (var port in portsToRemove)
+                        {
+                            if (_ports.Contains(port))
+                            {
+                                _ports.Remove(port);
+                                Removed?.Invoke(this, port);
+                                changed = true;
+                            }
+                        }
+
+                        if(changed)
+                        {
+                            EnumerationCompleted?.Invoke(this, _ports.ToList());
+                            changed = false;
+                        }
+
+                        Thread.Sleep(200);
+                    }
+
+                    Status = DeviceWatcherStatus.Stopped;
+                });
+                threadWatch.Priority = ThreadPriority.Lowest;
+                threadWatch.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            _started = false;
+            Status = DeviceWatcherStatus.Stopping;
+        }
+    }
+}
---
+++ nanoFramework.Tools.DebugLibrary.Shared/PortSerial/DeviceWatcherStatus.cs
@@ -0,0 +1,36 @@
+﻿namespace nanoFramework.Tools.Debugger.PortSerial
+{
+    public enum DeviceWatcherStatus
+    {
+        /// <summary>
+        /// The watcher has aborted operation. No subsequent events will be raised.
+        /// </summary>
+        Aborted = 5,
+
+        /// <summary>
+        /// This is the initial state of a Watcher object. During this state clients can register event handlers.
+        /// </summary>
+        Created = 0,
+
+        /// <summary>
+        /// The watcher has completed enumerating the initial collection. Items can still be added, updated or removed from the collection.
+        /// </summary>
+        EnumerationCompleted = 2,
+
+        /// <summary>
+        /// The watcher transitions to the Started state once Start is called. The watcher is enumerating the initial collection. Note that during this enumeration phase it is possible to receive Updated and Removed notifications but only to items that have already been Added.
+        /// </summary>
+        Started = 1,
+
+        /// <summary>
+        /// The client has called Stop and the watcher has completed all outstanding events. No further events will be raised.
+        /// </summary>
+        Stopped = 4,
+
+        /// <summary>
+        /// The client has called Stop and the watcher is still in the process of stopping. Events may still be raised.
+        /// </summary>
+        Stopping = 3
+
+    }
+}
