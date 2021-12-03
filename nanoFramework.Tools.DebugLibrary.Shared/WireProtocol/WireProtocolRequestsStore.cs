9cee11aea6070ce9379bbdf3212d6a593e463875
José Simões <jose.simoes@eclo.solutions>
2021-12-03 10:58:46 +0000

    Rework PerformSyncRequest (#335)
    ***NO_CI***

--- nanoFramework.Tools.DebugLibrary.Shared/WireProtocol/WireProtocolRequestsStore.cs
+++ nanoFramework.Tools.DebugLibrary.Shared/WireProtocol/WireProtocolRequestsStore.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace nanoFramework.Tools.Debugger.WireProtocol
 {
     public class WireProtocolRequestsStore
     {
         private readonly object _requestsLock = new object();
-        private readonly Dictionary<Tuple<uint, ushort>, WireProtocolRequest> _requests = new Dictionary<Tuple<uint, ushort>, WireProtocolRequest>();
+        private readonly Dictionary<Tuple<uint, ushort>, WireProtocolRequest> _requests = new();
 
         public void Add(WireProtocolRequest request)
         {
@@ -15,11 +19,12 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             {
                 // it's wise to check if this key is already on the dictionary
                 // can't use TryAdd because that's only available on the UWP API
-                var newKey = new Tuple<uint, ushort>(request.OutgoingMessage.Header.Cmd, request.OutgoingMessage.Header.Seq);
+                Tuple<uint, ushort> newKey = GetKey(request);
+
                 if (_requests.ContainsKey(newKey))
                 {
                     // remove the last one, before adding this
-                    _requests.Remove(newKey);
+                    _ = _requests.Remove(newKey);
                 }
 
                 _requests.Add(newKey, request);
@@ -31,6 +36,23 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             lock (_requestsLock)
             {
                 return _requests.Remove(new Tuple<uint, ushort>(header.Cmd, header.Seq));
+            }
+        }
+
+        public bool Remove(WireProtocolRequest request)
+        {
+            lock (_requestsLock)
+            {
+                if(_requests.Remove(GetKey(request)))
+                {
+                    request.RequestAborted();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
 
@@ -50,12 +72,9 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             }
         }
 
-        public ICollection<WireProtocolRequest> FindAllToCancel()
+        private Tuple<uint, ushort> GetKey(WireProtocolRequest request)
         {
-            lock (_requestsLock)
-            {
-                return _requests.Values.Where(x => x.Expires < DateTime.UtcNow || x.CancellationToken.IsCancellationRequested).ToList();
-            }
+            return new Tuple<uint, ushort>(request.OutgoingMessage.Header.Cmd, request.OutgoingMessage.Header.Seq);
         }
     }
 }
