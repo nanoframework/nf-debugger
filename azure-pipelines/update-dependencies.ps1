a2ce436962ae8995042e9681d155f3895a3e81de
José Simões <jose.simoes@eclo.solutions>
2021-12-21 17:12:30 +0000

    Work CI-CD
    - Update dependents now handles nf Deployer repo.
    
    ***NO_CI***

--- azure-pipelines/update-dependencies.ps1
+++ azure-pipelines/update-dependencies.ps1
@@ -1,4 +1,4 @@
-"Updating dependency at nf-Visual-Studio-extension" | Write-Host
+"Updating dependents of nano-debugger" | Write-Host
 
 # compute authorization header in format "AUTHORIZATION: basic 'encoded token'"
 # 'encoded token' is the Base64 of the string "nfbot:personal-token"
@@ -10,7 +10,6 @@ $auth = "basic $([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.G
 Start-Sleep -Seconds 60 
 
 # init/reset these
-$commitMessage = ""
 $prTitle = ""
 $newBranchName = "develop-nfbot/update-dependencies/" + [guid]::NewGuid().ToString()
 $packageTargetVersion = $env:NBGV_NuGetPackageVersion
@@ -22,6 +21,12 @@ Set-Location "$env:Agent_TempDirectory" | Out-Null
 # clone repo and checkout develop branch
 Write-Debug "Init and featch nf-Visual-Studio-extension repo"
 
+####################
+# VS 2019 & 2022
+
+"********************************************************************************" | Write-Host
+"Updating nanoFramework.Tools.Debugger.Net package in VS2019 & VS2022 solution..." | Write-Host
+
 git clone --depth 1 https://github.com/nanoframework/nf-Visual-Studio-extension repo
 Set-Location repo | Out-Null
 git config --global gc.auto 0
@@ -32,17 +37,10 @@ git config --global core.autocrlf true
 Write-Host "Checkout develop branch..."
 git checkout --quiet develop | Out-Null
 
-####################
-# VS 2019 & 2022
-
-Write-Host "Updating nanoFramework.Tools.Debugger.Net package in VS2019 & VS2022 solution..."
-
 dotnet remove VisualStudio.Extension-2019/VisualStudio.Extension-vs2019.csproj package nanoFramework.Tools.Debugger.Net
 dotnet add VisualStudio.Extension-2019/VisualStudio.Extension-vs2019.csproj package nanoFramework.Tools.Debugger.Net --prerelease
 dotnet remove VisualStudio.Extension-2022/VisualStudio.Extension-vs2022.csproj package nanoFramework.Tools.Debugger.Net
 dotnet add VisualStudio.Extension-2022/VisualStudio.Extension-vs2022.csproj package nanoFramework.Tools.Debugger.Net --prerelease
-
-#####################
 
 "Bumping nanoFramework.Tools.Debugger to $packageTargetVersion." | Write-Host -ForegroundColor Cyan                
 
@@ -59,6 +57,108 @@ $commitMessage += "### :warning: This is an automated update. Merge only after a
 
 Write-Debug "Git branch" 
 
+# check if anything was changed
+$repoStatus = "$(git status --short --porcelain)"
+
+if ($repoStatus -ne "")
+{
+    # create branch to perform updates
+    git branch $newBranchName
+
+    Write-Debug "Checkout branch" 
+
+    # checkout branch
+    git checkout $newBranchName
+
+    Write-Debug "Add changes" 
+
+    # commit changes
+    git add -A > $null
+
+    Write-Debug "Commit changed files"
+
+    git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
+
+    Write-Debug "Push changes"
+
+    git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
+
+    # start PR
+    # we are hardcoding to 'develop' branch to have a fixed one
+    # this is very important for tags (which don't have branch information)
+    # considering that the base branch can be changed at the PR ther is no big deal about this 
+    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="develop"} | ConvertTo-Json
+    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nf-Visual-Studio-extension/pulls"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $headers = @{}
+    $headers.Add("Authorization","$auth")
+    $headers.Add("Accept","application/vnd.github.symmetra-preview+json")
+
+    try 
+    {
+        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
+        'Started PR with dependencies update...' | Write-Host -NoNewline
+        'OK' | Write-Host -ForegroundColor Green
+    }
+    catch 
+    {
+        $result = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+
+        throw "Error starting PR: $responseBody"
+    }
+}
+else
+{
+    Write-Host "Nothing udpate at VS extension."
+}
+
+#######################
+# nanoFrameworkDeployer
+
+"**************************************************************************************" | Write-Host
+"Updating nanoFramework.Tools.Debugger.Net package in nanoFrameworkDeployer solution..." | Write-Host
+
+Set-Location "$env:Agent_TempDirectory" | Out-Null
+
+# clone repo and checkout develop branch
+Write-Debug "Init and featch nf-Deployer repo"
+
+git clone --depth 1 https://github.com/nanoframework/nanoFrameworkDeployer nanoFrameworkDeployer
+Set-Location nanoFrameworkDeployer | Out-Null
+git config --global gc.auto 0
+git config --global user.name nfbot
+git config --global user.email nanoframework@outlook.com
+git config --global core.autocrlf true
+
+Write-Host "Checkout develop branch..."
+git checkout --quiet develop | Out-Null
+
+dotnet remove nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net
+dotnet add nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net --prerelease
+
+"Bumping nanoFramework.Tools.Debugger to $packageTargetVersion." | Write-Host -ForegroundColor Cyan                
+
+# build commit message
+$commitMessage = "Bumps nanoFramework.Tools.Debugger to $packageTargetVersion.`n"
+# build PR title
+$prTitle = "Bumps nanoFramework.Tools.Debugger to $packageTargetVersion"
+
+# need this line so nfbot flags the PR appropriately
+$commitMessage += "`n[version update]`n`n"
+
+# add this to cascade updates
+$commitMessage += "`n`n***UPDATE_DEPENDENTS***`n`n"
+
+# better add this warning line               
+$commitMessage += "### :warning: This is an automated update. Merge only after all tests pass. :warning:`n"
+
+Write-Debug "Git branch" 
+
 # create branch to perform updates
 git branch $newBranchName
 
@@ -67,44 +167,54 @@ Write-Debug "Checkout branch"
 # checkout branch
 git checkout $newBranchName
 
-Write-Debug "Add changes" 
+# check if anything was changed
+$repoStatus = "$(git status --short --porcelain)"
 
-# commit changes
-git add -A > $null
-
-Write-Debug "Commit changed files"
-
-git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
-
-Write-Debug "Push changes"
-
-git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
-
-# start PR
-# we are hardcoding to 'develop' branch to have a fixed one
-# this is very important for tags (which don't have branch information)
-# considering that the base branch can be changed at the PR ther is no big deal about this 
-$prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="develop"} | ConvertTo-Json
-$githubApiEndpoint = "https://api.github.com/repos/nanoframework/nf-Visual-Studio-extension/pulls"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$headers = @{}
-$headers.Add("Authorization","$auth")
-$headers.Add("Accept","application/vnd.github.symmetra-preview+json")
-
-try 
+if ($repoStatus -ne "")
 {
-    $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
-    'Started PR with dependencies update...' | Write-Host -NoNewline
-    'OK' | Write-Host -ForegroundColor Green
+    Write-Debug "Add changes" 
+
+    # commit changes
+    git add -A > $null
+
+    Write-Debug "Commit changed files"
+
+    git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
+
+    Write-Debug "Push changes"
+
+    git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
+
+    # start PR
+    # we are hardcoding to 'develop' branch to have a fixed one
+    # this is very important for tags (which don't have branch information)
+    # considering that the base branch can be changed at the PR ther is no big deal about this 
+    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="develop"} | ConvertTo-Json
+    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nanoFrameworkDeployer/pulls"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $headers = @{}
+    $headers.Add("Authorization","$auth")
+    $headers.Add("Accept","application/vnd.github.symmetra-preview+json")
+
+    try 
+    {
+        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
+        'Started PR with dependencies update...' | Write-Host -NoNewline
+        'OK' | Write-Host -ForegroundColor Green
+    }
+    catch 
+    {
+        $result = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+
+        throw "Error starting PR: $responseBody"
+    }
 }
-catch 
+else
 {
-    $result = $_.Exception.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($result)
-    $reader.BaseStream.Position = 0
-    $reader.DiscardBufferedData()
-    $responseBody = $reader.ReadToEnd();
-
-    throw "Error starting PR: $responseBody"
+    Write-Host "Nothing udpate at nanoFramework Deployer."
 }
