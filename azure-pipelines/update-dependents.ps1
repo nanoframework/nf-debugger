30e777e2066b2e2518570e3e3c41af4d166a99ba
José Simões <jose.simoes@eclo.solutions>
2022-08-19 17:33:32 +0100

    Work CI-CD
    - Remove tag build.
    - Update task conditions accordingly.
    - Add nanoff to update dependents.
    
    ***NO_CI***

--- azure-pipelines/update-dependents.ps1
+++ azure-pipelines/update-dependents.ps1
@@ -139,7 +139,7 @@ Write-Host "Checkout main branch..."
 git checkout --quiet main | Out-Null
 
 dotnet remove nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net
-dotnet add nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net --prerelease
+dotnet add nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net
 
 "Bumping nanoFramework.Tools.Debugger to $packageTargetVersion." | Write-Host -ForegroundColor Cyan                
 
@@ -217,4 +217,108 @@ if ($repoStatus -ne "")
 else
 {
     Write-Host "Nothing udpate at nanoFramework Deployer."
+}
+
+#######################
+# nano firmware flasher
+
+"**************************************************************************************" | Write-Host
+"Updating nanoFramework.Tools.Debugger.Net package in nano firmware flasher solution..." | Write-Host
+
+Set-Location "$env:Agent_TempDirectory" | Out-Null
+
+# clone repo and checkout main branch
+Write-Debug "Init and featch nf-Deployer repo"
+
+git clone --depth 1 https://github.com/nanoframework/nanoFirmwareFlasher nanoFirmwareFlasher
+Set-Location nanoFirmwareFlasher | Out-Null
+git config --global gc.auto 0
+git config --global user.name nfbot
+git config --global user.email nanoframework@outlook.com
+git config --global core.autocrlf true
+
+Write-Host "Checkout main branch..."
+git checkout --quiet main | Out-Null
+
+dotnet remove nanoFirmwareFlasher.Library/nanoFirmwareFlasher.Library.csproj package nanoFramework.Tools.Debugger.Net
+dotnet add nanoFirmwareFlasher.Library/nanoFirmwareFlasher.Library.csproj package nanoFramework.Tools.Debugger.Net
+dotnet remove nanoFirmwareFlasher.Tool/nanoFirmwareFlasher.Tool.csproj package nanoFramework.Tools.Debugger.Net
+dotnet add nanoFirmwareFlasher.Tool/nanoFirmwareFlasher.Tool.csproj package nanoFramework.Tools.Debugger.Net
+
+"Bumping nanoFramework.Tools.Debugger to $packageTargetVersion." | Write-Host -ForegroundColor Cyan                
+
+# build commit message
+$commitMessage = "Bumps nanoFramework.Tools.Debugger to $packageTargetVersion.`n"
+# build PR title
+$prTitle = "Bumps nanoFramework.Tools.Debugger to $packageTargetVersion"
+
+# need this line so nfbot flags the PR appropriately
+$commitMessage += "`n[version update]`n`n"
+
+# add this to cascade updates
+$commitMessage += "`n`n***UPDATE_DEPENDENTS***`n`n"
+
+# better add this warning line               
+$commitMessage += "### :warning: This is an automated update. Merge only after all tests pass. :warning:`n"
+
+Write-Debug "Git branch" 
+
+# create branch to perform updates
+git branch $newBranchName
+
+Write-Debug "Checkout branch" 
+
+# checkout branch
+git checkout $newBranchName
+
+# check if anything was changed
+$repoStatus = "$(git status --short --porcelain)"
+
+if ($repoStatus -ne "")
+{
+    Write-Debug "Add changes" 
+
+    # commit changes
+    git add -A > $null
+
+    Write-Debug "Commit changed files"
+
+    git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
+
+    Write-Debug "Push changes"
+
+    git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
+
+    # start PR
+    # we are hardcoding to 'main' branch to have a fixed one
+    # this is very important for tags (which don't have branch information)
+    # considering that the base branch can be changed at the PR ther is no big deal about this 
+    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="main"} | ConvertTo-Json
+    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nanoFirmwareFlasher/pulls"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $headers = @{}
+    $headers.Add("Authorization","$auth")
+    $headers.Add("Accept","application/vnd.github.symmetra-preview+json")
+
+    try 
+    {
+        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
+        'Started PR with dependencies update...' | Write-Host -NoNewline
+        'OK' | Write-Host -ForegroundColor Green
+    }
+    catch 
+    {
+        $result = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+
+        throw "Error starting PR: $responseBody"
+    }
+}
+else
+{
+    Write-Host "Nothing udpate at nano firmware flasher."
 }
