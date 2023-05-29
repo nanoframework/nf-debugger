6c8b64afe359b3302724b0196f8589779cd1cfb7
José Simões <jose.simoes@eclo.solutions>
2023-05-29 13:04:36 +0100

    Work CI-CD
    - Remove update nano deployer from PS1.
    - Update script calling nuget update and restore for dependents.
    
    ***NO_CI***

--- azure-pipelines/update-dependents.ps1
+++ azure-pipelines/update-dependents.ps1
@@ -43,7 +43,7 @@ dotnet remove VisualStudio.Extension-2019/VisualStudio.Extension-vs2019.csproj p
 dotnet add VisualStudio.Extension-2019/VisualStudio.Extension-vs2019.csproj package nanoFramework.Tools.Debugger.Net --version $packageTargetVersion --no-restore 
 dotnet remove VisualStudio.Extension-2022/VisualStudio.Extension-vs2022.csproj package nanoFramework.Tools.Debugger.Net
 dotnet add VisualStudio.Extension-2022/VisualStudio.Extension-vs2022.csproj package nanoFramework.Tools.Debugger.Net --version $packageTargetVersion --no-restore 
-dotnet restore --force-evaluate
+nuget restore -uselockfile
 
 "Bumping nanoFramework.Tools.Debugger to v$packageTargetVersion." | Write-Host -ForegroundColor Cyan                
 
@@ -121,110 +121,6 @@ else
 }
 
 #######################
-# nanoFrameworkDeployer
-
-"**************************************************************************************" | Write-Host
-"Updating nanoFramework.Tools.Debugger.Net package in nanoFrameworkDeployer solution..." | Write-Host
-
-Set-Location "$env:Agent_TempDirectory" | Out-Null
-
-# clone repo and checkout main branch
-Write-Debug "Init and featch nf-Deployer repo"
-
-git clone --depth 1 https://github.com/nanoframework/nanoFrameworkDeployer nanoFrameworkDeployer
-Set-Location nanoFrameworkDeployer | Out-Null
-git config --global gc.auto 0
-git config --global user.name nfbot
-git config --global user.email nanoframework@outlook.com
-git config --global core.autocrlf true
-
-Write-Host "Checkout main branch..."
-git checkout --quiet main | Out-Null
-
-dotnet restore
-dotnet remove nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net 
-dotnet add nanoFrameworkDeployer/nanoFrameworkDeployer.csproj package nanoFramework.Tools.Debugger.Net --version $packageTargetVersion --no-restore 
-nuget restore -ForceEvaluate
-
-"Bumping nanoFramework.Tools.Debugger to v$packageTargetVersion." | Write-Host -ForegroundColor Cyan                
-
-# build commit message
-$commitMessage = "Bumps nanoFramework.Tools.Debugger to v$packageTargetVersion.`n"
-# build PR title
-$prTitle = "Bumps nanoFramework.Tools.Debugger to v$packageTargetVersion"
-
-# need this line so nfbot flags the PR appropriately
-$commitMessage += "`n[version update]`n`n"
-
-# add this to cascade updates
-$commitMessage += "`n`n***UPDATE_DEPENDENTS***`n`n"
-
-# better add this warning line               
-$commitMessage += "### :warning: This is an automated update. Merge only after all tests pass. :warning:`n"
-
-Write-Debug "Git branch" 
-
-# create branch to perform updates
-git branch $newBranchName
-
-Write-Debug "Checkout branch" 
-
-# checkout branch
-git checkout $newBranchName
-
-# check if anything was changed
-$repoStatus = "$(git status --short --porcelain)"
-
-if ($repoStatus -ne "")
-{
-    Write-Debug "Add changes" 
-
-    # commit changes
-    git add -A > $null
-
-    Write-Debug "Commit changed files"
-
-    git commit -m "$prTitle ***NO_CI***" -m "$commitMessage" > $null
-
-    Write-Debug "Push changes"
-
-    git -c http.extraheader="AUTHORIZATION: $auth" push --set-upstream origin $newBranchName > $null
-
-    # start PR
-    # we are hardcoding to 'main' branch to have a fixed one
-    # this is very important for tags (which don't have branch information)
-    # considering that the base branch can be changed at the PR ther is no big deal about this 
-    $prRequestBody = @{title="$prTitle";body="$commitMessage";head="$newBranchName";base="main"} | ConvertTo-Json
-    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nanoFrameworkDeployer/pulls"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-    $headers = @{}
-    $headers.Add("Authorization","$auth")
-    $headers.Add("Accept","application/vnd.github.symmetra-preview+json")
-
-    try 
-    {
-        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header $headers -ContentType "application/json" -Body $prRequestBody
-        'Started PR with dependencies update...' | Write-Host -NoNewline
-        'OK' | Write-Host -ForegroundColor Green
-    }
-    catch 
-    {
-        $result = $_.Exception.Response.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($result)
-        $reader.BaseStream.Position = 0
-        $reader.DiscardBufferedData()
-        $responseBody = $reader.ReadToEnd();
-
-        throw "Error starting PR: $responseBody"
-    }
-}
-else
-{
-    Write-Host "Nothing to udpate at nanoFramework Deployer."
-}
-
-#######################
 # nano firmware flasher
 
 "**************************************************************************************" | Write-Host
@@ -250,7 +146,7 @@ dotnet remove nanoFirmwareFlasher.Library/nanoFirmwareFlasher.Library.csproj pac
 dotnet add nanoFirmwareFlasher.Library/nanoFirmwareFlasher.Library.csproj package nanoFramework.Tools.Debugger.Net --version $packageTargetVersion --no-restore 
 dotnet remove nanoFirmwareFlasher.Tool/nanoFirmwareFlasher.Tool.csproj package nanoFramework.Tools.Debugger.Net
 dotnet add nanoFirmwareFlasher.Tool/nanoFirmwareFlasher.Tool.csproj package nanoFramework.Tools.Debugger.Net --version $packageTargetVersion --no-restore 
-dotnet restore --force-evaluate
+nuget restore -uselockfile
 
 "Bumping nanoFramework.Tools.Debugger to v$packageTargetVersion." | Write-Host -ForegroundColor Cyan                
 
