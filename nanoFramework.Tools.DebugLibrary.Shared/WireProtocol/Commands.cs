6d64755d5db10ba135ef9bb7da8fc6fd07629c05
José Simões <jose.simoes@eclo.solutions>
2023-06-08 09:12:07 +0100

    Update debug value class to match native counterpart

--- nanoFramework.Tools.DebugLibrary.Shared/WireProtocol/Commands.cs
+++ nanoFramework.Tools.DebugLibrary.Shared/WireProtocol/Commands.cs
@@ -1336,6 +1336,10 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
             }
         }
 
+        /////////////////////////////////////////////////////////////
+        // Keep in sync with Debugging_Value struct in native code //
+        /////////////////////////////////////////////////////////////
+
         public class Debugging_Value
         {
             public const uint HB_Alive = 0x01;
@@ -1364,6 +1368,9 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
 
             public uint m_td;
 
+            // For DATATYPE_GENERICINST
+            public uint m_ts;
+
             // For DATATYPE_SZARRAY
 
             public uint m_array_numOfElements;
@@ -1378,7 +1385,7 @@ namespace nanoFramework.Tools.Debugger.WireProtocol
 
             static internal Debugging_Value[] Allocate(int size, byte[] data)
             {
-                int num = size / (12 * 4 + 128);
+                int num = size / (13 * 4 + 128);
 
                 Debugging_Value[] res = new Debugging_Value[num];
 
